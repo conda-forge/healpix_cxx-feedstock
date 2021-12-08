6208f4946099a1de949fbcd5b19f18f7bac50fcf
Rhys Poulton <rhyspoulton92@gmail.com>
2021-12-08 16:17:14 +0000

    Added the -fPIC flag
    The -fPIC flag has been added to the cflags to ensure that the library can be easily
    shared

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,20 @@ cd src/common_libraries/libsharp
 
 ./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking
 
+if [ "$(uname)" == "Darwin" ]; then
+
+    sed -i '' 's/-O2/-O2 -fPIC/g' config.status
+    sed -i '' 's/-O3/-O3 -fPIC/g' config.status
+
+else
+
+    sed -i 's/-O2/-O2 -fPIC/g' config.status
+    sed -i 's/-O3/-O3 -fPIC/g' config.status
+
+fi
+
+./config.status
+
 make install -j ${CPU_COUNT}
 
 cd -
@@ -12,6 +26,6 @@ cd src/cxx
 export SHARP_CFLAGS="-I$PREFIX/include"
 export SHARP_LIBS="-L$PREFIX/lib -lsharp"
 
-./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking
+./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking --enable-shared
 
 make install -j ${CPU_COUNT}
