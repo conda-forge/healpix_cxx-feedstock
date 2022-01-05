fc841c13a169a8670c0fd48604116a7fbba4f6b9
Matthias Wittgen <wittgen@cern.ch>
2022-01-05 16:41:48 -0500

    patch to configure.ac is not needed

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,14 @@
 cd src/cxx/autotools
 
+# link and autoreconf are required to build alice
+# after patching configure.ac
 ln -s ../alice alice
-
 autoreconf --install
 
 export CPATH="${PREFIX}/include"
 
-./configure --prefix=$PREFIX --disable-dependency-tracking
+./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking --disable-static
 
-make -j ${CPU_COUNT}
-make install
+make -j ${CPU_COUNT} install
 
 
