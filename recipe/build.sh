dd754cfc88e958efe84a2b3fd66158e31a4b7e38
Alex Reustle <areustledev@gmail.com>
2022-04-13 17:38:00 -0400

    Support v3.31

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,34 +1,25 @@
-# Build libsharp
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./src/common_libraries/libsharp
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./src/cxx
-cd src/common_libraries/libsharp
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./src/cxx/autotools
+
+cd src/cxx/autotools
+
+# link and autoreconf are required to build alice
+# after patching configure.ac
+ln -s ../alice alice
+autoreconf --install
 
 # Add enable-pic flag so libsharp.a can be linked into libhealpix_cxx later
-./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking --disable-shared --enable-pic
-
-make install -j ${CPU_COUNT}
-
-cd -
-
-cd src/cxx
-
-# Build libhealpix_cxx
-export SHARP_CFLAGS="-I$PREFIX/include"
-export SHARP_LIBS="-L$PREFIX/lib -lsharp"
-
 ./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking --disable-static
 
+export EXTERNAL_CFITSIO=yes  
+export CFITSIO_EXT_LIB=${PREFIX}/lib  
+export CFITSIO_EXT_INC=${PREFIX}/include  
+
 make install -j ${CPU_COUNT}
 
-cd -
+find . -name '*.h' -exec cp --parents {} ${PREFIX}/include/healpix_cxx/ \;
 
 # delete all libsharp files as they are not needed
-rm -f  $PREFIX/lib/libsharp.a
-mkdir -p $PREFIX/include/healpix_cxx
-mv $PREFIX/include/libsharp $PREFIX/include/healpix_cxx
-rm -rf $PREFIX/lib/pkgconfig/libsharp.pc
-
-
-# Copy and rename the libsharp lisence
-cp src/common_libraries/libsharp/COPYING COPYING-libsharp
+rm -f $PREFIX/lib/libsharp.a
+rm -rf $PREFIX/include/libsharp
+rm -f $PREFIX/lib/pkgconfig/libsharp.pc
