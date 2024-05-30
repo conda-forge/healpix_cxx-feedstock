99353d0038c8f27af4360d8880c93fbf7643ad2a
Leo Singer <leo.singer@ligo.org>
2024-05-30 12:39:17 -0400

    Update to 3.82.0
    - Unbundle libsharp
    - Use standalone C++ archive source archive instead of monolithic
      tarball
    - Build shared libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,34 +1,8 @@
 # Build libsharp
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./src/common_libraries/libsharp
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./src/cxx
-cd src/common_libraries/libsharp
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 # Add enable-pic flag so libsharp.a can be linked into libhealpix_cxx later
-./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking --disable-shared --enable-pic
+./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking
 
 make install -j ${CPU_COUNT}
-
-cd -
-
-cd src/cxx
-
-# Build libhealpix_cxx
-export SHARP_CFLAGS="-I$PREFIX/include"
-export SHARP_LIBS="-L$PREFIX/lib -lsharp"
-
-./configure --prefix=$PREFIX --disable-silent-rules --disable-dependency-tracking --disable-static
-
-make install -j ${CPU_COUNT}
-
-cd -
-
-# delete all libsharp files as they are not needed
-rm -f  $PREFIX/lib/libsharp.a
-mkdir -p $PREFIX/include/healpix_cxx
-mv $PREFIX/include/libsharp $PREFIX/include/healpix_cxx
-rm -rf $PREFIX/lib/pkgconfig/libsharp.pc
-
-
-# Copy and rename the libsharp lisence
-cp src/common_libraries/libsharp/COPYING COPYING-libsharp
